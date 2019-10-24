8f21cf30cbb56dc2f042ac930811bbe72ad8f48b
Murad Mammadov <mumamm@ttu.ee>
2019-10-24 15:25:03 +0200

    Refactoring

--- src/components/EntityComponent.jsx
+++ src/components/EntityComponent.jsx
@@ -1,48 +1,84 @@
 import React, { Component } from "react";
 import {
-  ComposedChart,
-  Area,
+  XYPlot,
   XAxis,
   YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from "recharts";
+  VerticalGridLines,
+  HorizontalGridLines,
+  AreaSeries
+} from "react-vis";
 
 class EntityComponent extends Component {
   fetchData() {
-    let pureEntities = this.props.entities;
+    if (this.props.entities !== undefined) {
+      let entities = this.props.entities;
+      let menData = entities[1];
+      let womenData = entities[0];
+      let maxVal = 0;
 
-    console.log("Here:");
-    this.entities = [];
-    if (pureEntities.length !== 0) {
-      for (let i = 8; i < 19; i++) {
-        this.entities.push({
-          name: "" + i - 8,
-          man: Object.values(pureEntities[0])[i],
-          woman: Object.values(pureEntities[1])[i]
-        });
+      if (menData !== undefined && womenData !== undefined) {
+        menData = this.clearData(menData);
+        womenData = this.clearData(womenData);
+        maxVal = this.getMax(menData, womenData);
+
+        this.XAxis = this.createXAxis(maxVal / 10);
+
+        let menArray = [...new Map(Object.entries(menData)).values()];
+        let womenArray = [...new Map(Object.entries(womenData)).values()];
+
+        this.dataMen = this.distributeData(this.XAxis, menArray);
       }
     }
+  }
+
+  clearData(data) {
+    let { mean, p1, p2, p3, p4, p6, p7, p8, p9, minVal, maxVal } = data;
+
+    return { p1, p2, p3, p4, mean, p6, p7, p8, p9, minVal, maxVal };
+  }
+
+  createXAxis(step) {
+    return Array.from(Array(10), (x, i) => Math.floor(i * step));
+  }
+
+  getMax(men, women) {
+    let maxMen = men.maxVal;
+    let maxWomen = women.maxVal;
+    let max = Math.max(maxMen, maxWomen);
+    return max;
+  }
+
+  distributeData(XAxis, data) {
+    let entities = Array(10).fill(0);
+    for (let i = 0; i < 10; i++) {
+      entities[Math.floor(data[i] / XAxis[1])]++;
+    }
+
+    console.log(entities);
+    let dict = [];
+    for (let i = 0; i < 10; i++) {
+      dict.push({ x: XAxis[i], y: entities[i] });
+    }
+
+    return dict;
   }
 
   render() {
     this.fetchData();
     return (
-      <ComposedChart
-        width={1000}
-        height={500}
-        data={this.entities}
-        margin={{ top: 20, right: 80, bottom: 20, left: 20 }}
-      >
-        <XAxis dataKey="name" />
-        <YAxis label={{ value: "Wage", angle: -90, position: "insideLeft" }} />
-        <Legend />
-        <CartesianGrid stroke="#f5f5f5" />
-
-        <Area type="monotone" dataKey="woman" fill="#FF0000" stroke="#8884d8" />
-        <Area type="monotone" dataKey="man" fill="#8884d8" stroke="#8884d8" />
-      </ComposedChart>
+      <div>
+        <XYPlot width={300} height={300}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <AreaSeries
+            className="area-series-example"
+            curve="curveNatural"
+            data={this.dataMen}
+          />
+        </XYPlot>
+      </div>
     );
   }
 }
