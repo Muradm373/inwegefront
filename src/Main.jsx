aca0beafc181e3780d3a229e2b9bf3300cf4ee9b
Murad Mammadov <muradm373@gmail.com>
2020-08-27 18:51:40 +0300

    Header style changed

--- src/Main.jsx
+++ src/Main.jsx
@@ -6,13 +6,7 @@ import SalaryCalculator from "./components/salary-calculator-page/SalaryCalculat
 import Feedback from "./components/salary-calculator-page/selector-components/Feedback";
 import SubscriptionComponent from "./components/subscription-headline/SubscriptionComponent";
 import WageForecast from "./components/wage-forecast-page/WageForecast";
-import changeLanguage, {
-  about, APP_NAME,
-  main, tabs
-} from "./dictionary/text";
-
-
-
+import changeLanguage, { about, APP_NAME, main, tabs } from "./dictionary/text";
 
 class Main extends Component {
   constructor(props) {
@@ -38,8 +32,6 @@ class Main extends Component {
 
     return lang;
   }
-
-
 
   scrollToBottom() {
     this.messagesEnd.scrollIntoView({ behavior: "smooth" });
@@ -67,12 +59,11 @@ class Main extends Component {
     if (this.state.menu === tabs[1]) {
       return (
         <div>
-        <WageForecast
-          onDataChange={this.getData}
-          mapElementColor={this.state.mapElementColor}
-        />
-      </div>
-      
+          <WageForecast
+            onDataChange={this.getData}
+            mapElementColor={this.state.mapElementColor}
+          />
+        </div>
       );
     }
     if (this.state.menu === tabs[3]) {
@@ -84,13 +75,13 @@ class Main extends Component {
       );
     } else {
       return (
-      <div>
-        <SalaryCalculator
-          id="graph"
-          onDataChange={this.getData}
-          mapElementColor={this.state.mapElementColor}
-        ></SalaryCalculator>
-      </div>
+        <div>
+          <SalaryCalculator
+            id="graph"
+            onDataChange={this.getData}
+            mapElementColor={this.state.mapElementColor}
+          ></SalaryCalculator>
+        </div>
       );
     }
   }
@@ -109,7 +100,7 @@ class Main extends Component {
             </div>
             <div className="site-mobile-menu-body"></div>
           </div>
-          <header
+          {/* <header
             className="site-navbar js-sticky-header site-navbar-target"
             role="banner"
             style={{ backgroundColor: "black", color: "white" }}
@@ -165,25 +156,6 @@ class Main extends Component {
                       </li>
                       <li>
                             
-                            <div className="flex-list wpml-ls-statics-shortcode_actions wpml-ls wpml-ls-legacy-list-horizontal">
-                              <ul>
-                                <li className="wpml-ls-slot-shortcode_actions wpml-ls-item wpml-ls-item-et wpml-ls-first-item wpml-ls-item-legacy-list-horizontal">
-                                    <a href="/ee" className="wpml-ls-link" onClick={()=>this.refresh("es")}>
-                                      <span className="wpml-ls-native"  style={{color: this.defaultValue() === "es"? '#4e73df' : ''}}>EE</span>
-                                    </a>
-                                  </li>
-                                  <li className="wpml-ls-slot-shortcode_actions wpml-ls-item wpml-ls-item-en wpml-ls-current-language wpml-ls-last-item wpml-ls-item-legacy-list-horizontal">
-                                    <a href="/" onClick={()=>this.refresh("en")} className="wpml-ls-link">
-                                      <span className="wpml-ls-native" style={{color: this.defaultValue() === "en"? '#4e73df' : ''}}>EN</span>
-                                    </a>
-                                  </li>
-                                  <li className="flex-list-lst wpml-ls-slot-shortcode_actions wpml-ls-item wpml-ls-item-en wpml-ls-current-language wpml-ls-last-item wpml-ls-item-legacy-list-horizontal">
-                                    <a href="/ru" onClick={()=>this.refresh("ru")} className="wpml-ls-link">
-                                      <span className="wpml-ls-native" style={{color: this.defaultValue() === "ru"? '#4e73df' : ''}}>RU</span>
-                                    </a>
-                                  </li>
-                              </ul>
-                            </div>
 
                       </li>
                     </ul>
@@ -200,6 +172,342 @@ class Main extends Component {
                     <span></span>
                   </a>
                 </div>
+              </div>
+            </div>
+          </header>*/}
+          <header class="layout__header">
+            <div class="layout__header__top">
+              <div class="content-link">
+                <a href="#main">Navigate to content</a>
+              </div>
+              <div class="u-container">
+                <div class="layout__header__top__left">
+                  <nav
+                    role="navigation"
+                    aria-labelledby="block-rakendustemenuu-menu"
+                    id="block-rakendustemenuu"
+                    class="block block-menu navigation menu--app-menu"
+                  >
+                    <span
+                      id="block-rakendustemenuu-menu"
+                      class="menu--app-menu__title"
+                    >
+                      Statistics Estonia:
+                    </span>
+
+                    <ul data-region="header_top_left" class="menu">
+                      <li class="menu-item menu-item--expanded">
+                        <span tabindex="0">Webpage</span>
+                        <ul class="menu">
+                          <li class="menu-item">
+                            <a
+                              href="http://pub.stat.ee/px-web.2001/dialog/statfile1.asp"
+                              target="_blank"
+                            >
+                              Statistical database PxWeb
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a
+                              href="http://andmebaas.stat.ee/?lang=en&amp;SubSessionId=d6bed41d-cf9f-44f2-975b-4680e7060ae1&amp;themetreeid=-200"
+                              target="_blank"
+                            >
+                              Statistical database .Stat
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a
+                              href="https://estat.stat.ee/sa-auth/login?TARGET=https%3A//estat.stat.ee/valisportaal/j_spring_cas_security_check&amp;language=en"
+                              target="_blank"
+                            >
+                              eSTAT
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a
+                              href="https://juhtimislauad.stat.ee/"
+                              target="_blank"
+                              title="https://juhtimislauad.stat.ee/"
+                            >
+                              Dashboards
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a
+                              href="https://tamm.stat.ee/?lang=en"
+                              target="_blank"
+                            >
+                              Tree of Truth
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a
+                              href="https://estat.stat.ee/StatistikaKaart/VKR"
+                              target="_blank"
+                            >
+                              Statistics map application
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a
+                              href="https://valiskaubandus.stat.ee/?locale=en"
+                              target="_blank"
+                            >
+                              Foreign trade application
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a
+                              href="https://ametipalk.stat.ee/"
+                              target="_blank"
+                            >
+                              Wages and salaries application
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a href="/nimed/" target="_blank">
+                              Name statistics application
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+                <div class="layout__header__top__center">
+                  <nav
+                    role="navigation"
+                    aria-labelledby="block-sotsiaalmenuu-menu"
+                    id="block-sotsiaalmenuu"
+                    class="block block-menu navigation menu--social-menu"
+                  >
+                    <h2 class="visually-hidden" id="block-sotsiaalmenuu-menu">
+                      Sotsiaalmenüü{" "}
+                    </h2>
+
+                    <ul class="menu menu-level-0">
+                      <li class="menu-item">
+                        <a
+                          href="https://et-ee.facebook.com/Statistikaamet"
+                          target="_blank"
+                          class="fa-facebook-f"
+                        >
+                          Facebook
+                        </a>
+
+                        <div class="menu_link_content menu-link-contentsocial-menu view-mode-default menu-dropdown menu-dropdown-0 menu-type-default">
+                          <div class="field field--name-field-tooltip field--type-string-long field--label-hidden field__items">
+                            <div class="field__item">Facebook</div>
+                          </div>
+                        </div>
+                      </li>
+
+                      <li class="menu-item">
+                        <a
+                          href="https://twitter.com/eestistatistika"
+                          target="_blank"
+                          class="fa-twitter"
+                        >
+                          Twitter
+                        </a>
+
+                        <div class="menu_link_content menu-link-contentsocial-menu view-mode-default menu-dropdown menu-dropdown-0 menu-type-default">
+                          <div class="field field--name-field-tooltip field--type-string-long field--label-hidden field__items">
+                            <div class="field__item">Twitter</div>
+                          </div>
+                        </div>
+                      </li>
+
+                      <li class="menu-item">
+                        <a
+                          href="https://www.linkedin.com/company/statistikaamet-statistics-estonia/"
+                          target="_blank"
+                          class="fa-linkedin"
+                        >
+                          LinkedIn
+                        </a>
+
+                        <div class="menu_link_content menu-link-contentsocial-menu view-mode-default menu-dropdown menu-dropdown-0 menu-type-default">
+                          <div class="field field--name-field-tooltip field--type-string-long field--label-hidden field__items">
+                            <div class="field__item">LinkedIn</div>
+                          </div>
+                        </div>
+                      </li>
+
+                      <li class="menu-item">
+                        <a
+                          href="https://www.slideshare.net/Statistikaamet"
+                          target="_blank"
+                          class="fa-slideshare"
+                        >
+                          Slideshare
+                        </a>
+
+                        <div class="menu_link_content menu-link-contentsocial-menu view-mode-default menu-dropdown menu-dropdown-0 menu-type-default">
+                          <div class="field field--name-field-tooltip field--type-string-long field--label-hidden field__items">
+                            <div class="field__item">Slideshare</div>
+                          </div>
+                        </div>
+                      </li>
+
+                      <li class="menu-item">
+                        <a
+                          href="https://www.youtube.com/user/Statistikaamet/featured"
+                          target="_blank"
+                          class="fa-youtube"
+                        >
+                          Youtube
+                        </a>
+
+                        <div class="menu_link_content menu-link-contentsocial-menu view-mode-default menu-dropdown menu-dropdown-0 menu-type-default">
+                          <div class="field field--name-field-tooltip field--type-string-long field--label-hidden field__items">
+                            <div class="field__item">Youtube</div>
+                          </div>
+                        </div>
+                      </li>
+
+                      <li class="menu-item">
+                        <a
+                          href="https://www.instagram.com/eesti_statistika/"
+                          target="_blank"
+                          class="fa-instagram"
+                        >
+                          Instagram
+                        </a>
+
+                        <div class="menu_link_content menu-link-contentsocial-menu view-mode-default menu-dropdown menu-dropdown-0 menu-type-default">
+                          <div class="field field--name-field-tooltip field--type-string-long field--label-hidden field__items">
+                            <div class="field__item">Instagram</div>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+                <div class="layout__header__top__right">
+                  <nav
+                    role="navigation"
+                    aria-labelledby="block-paiseviited-menu"
+                    id="block-paiseviited"
+                    class="block block-menu navigation menu--header-references"
+                  >
+                    <div className="flex-list wpml-ls-statics-shortcode_actions wpml-ls wpml-ls-legacy-list-horizontal">
+                      <ul>
+                        <li className="wpml-ls-slot-shortcode_actions wpml-ls-item wpml-ls-item-et wpml-ls-first-item wpml-ls-item-legacy-list-horizontal">
+                          <a
+                            href="/ee"
+                            className="wpml-ls-link"
+                            onClick={() => this.refresh("es")}
+                          >
+                            <span
+                              className="wpml-ls-native"
+                              style={{
+                                color:
+                                  this.defaultValue() === "es" ? "#4e73df" : "",
+                              }}
+                            >
+                              EE
+                            </span>
+                          </a>
+                        </li>
+                        <li className="wpml-ls-slot-shortcode_actions wpml-ls-item wpml-ls-item-en wpml-ls-current-language wpml-ls-last-item wpml-ls-item-legacy-list-horizontal">
+                          <a
+                            href="/"
+                            onClick={() => this.refresh("en")}
+                            className="wpml-ls-link"
+                          >
+                            <span
+                              className="wpml-ls-native"
+                              style={{
+                                color:
+                                  this.defaultValue() === "en" ? "#4e73df" : "",
+                              }}
+                            >
+                              EN
+                            </span>
+                          </a>
+                        </li>
+                        <li className="flex-list-lst wpml-ls-slot-shortcode_actions wpml-ls-item wpml-ls-item-en wpml-ls-current-language wpml-ls-last-item wpml-ls-item-legacy-list-horizontal">
+                          <a
+                            href="/ru"
+                            onClick={() => this.refresh("ru")}
+                            className="wpml-ls-link"
+                          >
+                            <span
+                              className="wpml-ls-native"
+                              style={{
+                                color:
+                                  this.defaultValue() === "ru" ? "#4e73df" : "",
+                              }}
+                            >
+                              RU
+                            </span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+            <div class="layout__header__bottom">
+              <div class="u-container">
+                <div
+                  id="block-stat-branding"
+                  class="block block-system block-system-branding-block"
+                >
+                  <a href="/" rel="home" class="site-logo">
+                    <img
+                      src={require("./resources/icon.png")}
+                      alt="Home"
+                      style={{ width: "120px" }}
+                    />
+                  </a>
+                </div>
+                <nav
+                  role="navigation"
+                  aria-labelledby="block-mainnavigationenglish-menu"
+                  id="block-mainnavigationenglish"
+                  class="block block-menu navigation menu--main-en"
+                >
+                  <ul data-region="header_bottom" class="menu">
+                    <li class="menu-item menu-item--expanded">
+                      <span
+                        tabindex="0"
+                        onClick={this.changeMenu}
+                        style={{
+                          color: this.state.menu === tabs[1] ? "#4e73df" : "",
+                        }}
+                      >
+                        {tabs[1]}
+                      </span>
+                    </li>
+                    <li class="menu-item menu-item--expanded">
+                      <span
+                        tabindex="1"
+                        onClick={this.changeMenu}
+                        style={{
+                          color: this.state.menu === tabs[2] ? "#4e73df" : "",
+                        }}
+                      >
+                        {tabs[2]}
+                      </span>
+                    </li>
+                    <li class="menu-item menu-item--expanded">
+                      <span
+                        tabindex="2"
+                        className="menuSpan"
+                        onClick={this.changeMenu}
+                        style={{
+                          color: this.state.menu === tabs[3] ? "#4e73df" : "",
+                        }}
+                      >
+                        {tabs[3]}
+                      </span>
+                    </li>
+                  </ul>
+                </nav>
+
               </div>
             </div>
           </header>
